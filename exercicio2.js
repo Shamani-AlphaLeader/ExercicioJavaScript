1fd4e22007a6ba3e268bca7c81a77ac0c321fff6
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-05-03 14:57:58 -0300

    feat:Exercicio 2 java script

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+let dia = parseInt ( prompt("Insira um numero de 1 a 7") )
+switch (dia) {
+    case 1: alert ("Segunda feira")
+    case 2: alert ("Terça feira")
+    case 3: alert ("Quarta feira")
+    case 4: alert ("Quinta feira")
+    case 5: alert ("Sexta feira")
+    case 6: alert ("Sabado")
+    case 7: alert ("Domingo")
+}
