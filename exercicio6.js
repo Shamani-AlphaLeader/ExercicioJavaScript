d49bc7d81354ec1b3cde701916cf803db9d949a6
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-05-03 16:59:04 -0300

    feat: Exercicio 6 java script

---
+++ exercicio6.js
@@ -0,0 +1,9 @@
+let nascimento = parseInt(prompt("Digite o seu ano de nascimento:"))
+
+let idade = 2024-nascimento;
+    
+    if( idade >=18 ) {
+        alert("Sua idade foi aprovada!!!");
+    } else {
+        alert("Voce não tem permissão de acesso, devido a restrições de idade!")
+    }
