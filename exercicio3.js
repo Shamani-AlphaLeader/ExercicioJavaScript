1ee24bf00fd6c5e4726cb24cd3358dc94a2f18af
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-05-03 15:22:59 -0300

    feat: Exercicio 3 java script

---
+++ exercicio3.js
@@ -0,0 +1,9 @@
+let numero = prompt ("Digite um numero para a sua identificação")
+
+if (numero%2 == 0 ) {
+    alert("Este é um número par")
+}
+  else {
+    alert("Este é um número ímpar")
+  }
+  
