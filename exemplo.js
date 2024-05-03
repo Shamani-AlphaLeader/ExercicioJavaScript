84cda828016625a93dbc6ceada2ea6f7b64825c4
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-05-03 14:40:38 -0300

    feat: Exercicio 1 java script

--- exemplo.js
+++ exemplo.js
@@ -7,21 +7,50 @@
 // Concatenação
 // console.log("Seja bem vindo " + nome + "!!!");
 
-let pontos = 100
+//let pontos = 100
 
 //parseInt() -> Transforma oque esta entre () em numero inteiro.
 //let bonus = parseInt ( "Quantos pontos voce deseja somar?") )
 
-//Adicao
-//Subtracao
-//Multiplicacao
-//Divisao
+//Adicao ->
+//Subtracao ->
+//Multiplicacao ->
+//Divisao ->
 
+// == -> verifica se um valor é igual a outro. -> true
+// === -> verifica se o valor e o tipo dos itens são iguais. -> false
 
-let bonus = parseInt( prompt("Quantos pontos voce deseja somar?") )
+//let bonus = parseInt( prompt("Quantos pontos voce deseja somar?") )
+
+//let pontuacaoTotal = pontos + bonus
+//console.log(pontuacaoTotal)
+
+//parseFloat() -> Transforma oque esta em () em numero com , (numero quebrado ou nao inteiro).
+//let notaAluno = parseFloat (prompt ("Digite a nota do aluno") )
+
+// Escreva um programa que solicite o nome do usuário e lhe dê boas vindas, 
+// exibindo uma mensagem pelo console.
+
+// prompt() -> Recebe uma entrada digitada
+let nome = prompt("Olá, digite seu nome: ")
+
+// Concatenação
+console.log("Seja bem vindo " + nome + "!!!");
+
+let pontos = 100
+
+// parseInt() -> Transforma o que está entre () em número inteiro.
+let bonus =  parseInt( prompt("Quantos pontos você deseja somar?") )
+
+// Adição -> num1 + num2
+// Subtração -> num1 - num2
+// Multiplicação -> num1 * num2
+// Divisão -> num1 / num2
 
 let pontuacaoTotal = pontos + bonus
 console.log(pontuacaoTotal)
 
-//parseFloat() -> Transforma oque esta em () em numero com , (numero quebrado ou nao inteiro).
-let notaAluno = parseFloat (prompt ("Digite a nota do aluno") )
+// parseFloat() -> Transforma o que está entre () em número com , (quebrado)
+let notaAluno = parseFloat(prompt("Digite a nota do aluno: "))
+
+console.log(notaAluno);
