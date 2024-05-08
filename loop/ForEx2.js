dd30e4180228ddedbc6b15ce9011fc8910d40ab3
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-05-08 15:30:20 -0300

    feat: exercicio for2 concluido

--- loop/ForEx2.js
+++ loop/ForEx2.js
@@ -1,1 +1,9 @@
 //	Imprimir números pares de 1 a 10.
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let numerosPares = numeros.filter(function(numero) {
+  return numero % 2 === 0;
+});
+
+console.log(numerosPares);
